08d2fa2c0e692a38474072254443eac8e7f970db
hameedshaik555 <88450801+hameedshaik555@users.noreply.github.com>
2021-08-20 00:18:26 +0530

    Create operator.c

---
+++ operator.c
@@ -0,0 +1,33 @@
+  
+
+
+ #include<stdio.h>
+int main()
+{
+int a=54 , b=34;
+float c=3.45 , d=4.562;
+printf("sum=%d\n", a+b); 
+printf("diff=%d\n", a-b); 
+printf("mul=%d\n", a*b); 
+printf("div=%d\n", a/b); 
+printf("mod=%d\n", a%b); 
+printf("%d\n" ,c>d); 
+printf("%d\n" ,c<d); 
+printf("%d\n" ,c>=d); 
+printf("%d\n" ,c<=d); 
+printf("%d\n" ,c==d); 
+printf("%d\n" ,c!=d); 
+printf("%d\n", a&b); 
+printf("%d\n", a|b); 
+printf("%d\n", a^b); 
+printf("%d\n", a<<b);
+printf("%d\n", a>>b); 
+printf("%d\n", a&&b); 
+printf("%d\n", a||b); 
+printf("%d\n", !a); 
+printf("%d\n", a+=b); 
+printf("%d\n", a-=b); 
+printf("%d\n", a*=b); 
+printf("%d\n", a/=b); 
+printf("%d\n", a%=b); 
+}
